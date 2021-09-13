a7fd4d318f8ec1be449c37fa3d4431cbd9640123
Sergey Novikov <sergey.novikov@flaconi.de>
2021-09-13 20:56:03 +0200

    Add versions

--- versions.tf
+++ versions.tf
@@ -1,4 +1,13 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1"
+    }
+  }
+  required_version = ">= 0.12.26"
 }
